61cdf8e9aaf574931ef8aa64645db352e4bff677
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-09-14 07:39:42 -0500

    Basic combat started

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPSC;
+    [SerializeField] float range = 100f;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPSC.transform.position, FPSC.transform.forward, out hit, range);
+        Debug.Log("I hit this: " + hit.transform.name);
+    }
+}
