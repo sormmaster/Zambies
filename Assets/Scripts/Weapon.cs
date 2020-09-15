94a886cc66e28499b28c104c3e14743b300e1e68
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-09-15 08:15:10 -0500

    Enemy Health and Muzzle Flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPSC;
     [SerializeField] float range = 100f;
+    [SerializeField] float baseDamage = 25f;
+    [SerializeField] ParticleSystem flash;
 
 
     void Update()
@@ -18,8 +21,26 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        CastToHit();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        flash.Play();
+    }
+
+    private void CastToHit()
+    {
         RaycastHit hit;
-        Physics.Raycast(FPSC.transform.position, FPSC.transform.forward, out hit, range);
-        Debug.Log("I hit this: " + hit.transform.name);
+        if (Physics.Raycast(FPSC.transform.position, FPSC.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this: " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target)
+            {
+                target.gotHit(baseDamage);
+            }
+        }
     }
 }
