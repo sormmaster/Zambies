70b690fb4ffd945d6a162928a5aefaf1ad51bfd9
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-09-16 22:53:39 -0500

    Game Over Screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+public void ReloadGame()
+    {
+        Debug.Log("lets play again");
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
