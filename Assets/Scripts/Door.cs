f32400366ca50f36282f06ddd12ed53d863063c5
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-12-19 13:45:33 -0600

    Buildings Doors Keys

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string keyName = "default";
+    Vector3 closed;
+    Vector3 openUp;
+    public void Start()
+    {
+        closed = transform.position;
+        openUp = new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
+    }
+    public void open(string keyPickedUp)
+    {
+        if(keyName == keyPickedUp)
+        {
+            StartCoroutine(opening());
+        }
+    }
+
+    IEnumerator opening()
+    {
+        
+        while(transform.position != openUp)
+        {
+            transform.position = Vector3.Lerp(transform.position, openUp, Time.deltaTime);
+            yield return Time.deltaTime;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] string name = "default";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var couldPickup = FindObjectsOfType<Door>();
+            foreach(Door gate in couldPickup)
+            {
+                gate.open(name);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
+
